d32eddb4df34b8e9e0f2c74e6e0b8a9018e5d5f5
Slasnista <stan.chambon@gmail.com>
2018-04-23 11:50:06 -0700

    added function to compute start ends durations

---
+++ give_starts_ends.m
@@ -0,0 +1,13 @@
+function [starts, ends, durations] = give_starts_ends(pred)
+
+% evaluate starts / ends / durations
+pred_starts_ends = pred(1, 2:end) - pred(1, 1:end - 1);
+
+idx_starts = find(pred_starts_ends==1);
+idx_ends = find(pred_starts_ends==-1);
+
+starts = idx_starts / sfreq;
+ends = idx_ends / sfreq;
+durations = ends - starts;
+
+end
