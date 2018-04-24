b528a010f28f37401943e90b1391512c231a08e1
Slasnista <stan.chambon@gmail.com>
2018-04-23 18:03:59 -0700

    running loop crash test

--- prepare_scores.py
+++ prepare_scores.py
@@ -2,27 +2,29 @@ from scipy.io import loadmat
 import pandas as pd
 
 
+record = "01-02-0002"
 f = loadmat("scores_01-02-0002.mat")
 
 scores = []
-fi = f["metrics"][0, 0]
 
-precision = fi[0][0]
-recall = fi[1][0]
-f1 = fi[2][0]
-iou_th = fi[3][0]
-lam3 = fi[4][0]
-threshold = fi[5][0]
-record = str(fi[6][0])
+for i in range(f["metrics"].shape[0]):
+    fi = f["metrics"][i, 0]
+
+    precision = fi[0][0]
+    recall = fi[1][0]
+    f1 = fi[2][0]
+    iou_th = fi[3][0]
+    lam3 = fi[4][0]
+    threshold = fi[5][0]
 
     s = pd.DataFrame()
-    s["precision"] = precision.squeeze()[:-1]
-    s["recall"] = recall.squeeze()[:-1]
-    s["f1"] = f1.squeeze()[:-1]
+    s["precision"] = precision.squeeze()
+    s["recall"] = recall.squeeze()
+    s["f1"] = f1.squeeze()
     s["iou_th"] = iou_th.squeeze()
     s["lam3"] = lam3.squeeze()
     s["threshold"] = threshold.squeeze()
-    s["record"] = record.squeeze()
+    s["record"] = record
 
     scores.append(s)
 
